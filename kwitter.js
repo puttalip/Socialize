2dbf6a8a59a0165e2dfae6319e5b6b518b27a923
puttalip <82786508+puttalip@users.noreply.github.com>
2022-04-30 16:08:00 -0400

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,8 +1,11 @@
 function addUser()
 {
-    user_name = document.getElementById("user_name").ariaValueMax;
+    user_name = document.getElementById("user_name").value;
 
-    localStorage.setItem("user_name", user_name);
+    firebase.database().rel("/").child(space_name).update({
+        purpose : "adding user"
+
+    });
 
     window.location = "kwitter_room.html";
 }
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,44 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyAwEiPvzIe3ToAj4dT5U-EtT-h3GoeS2y8",
+      authDomain: "socialize-189b6.firebaseapp.com",
+      databaseURL: "https://socialize-189b6-default-rtdb.firebaseio.com",
+      projectId: "socialize-189b6",
+      storageBucket: "socialize-189b6.appspot.com",
+      messagingSenderId: "253090893536",
+      appId: "1:253090893536:web:092d8c31818a15f7f28a85",
+      measurementId: "G-WBLC8PCR71"
+    };
+    
+    var app = initializeApp(firebaseConfig);
 
-//ADD YOUR FIREBASE LINKS HERE
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+purpose : "adding room name"
+      });
+      
+      localStorage.setItem("room _name", room_name);
+
+      window.location = "kwitter_page.html"; 
+}
+
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      //Start code
-
-      //End code
+       console.log("Room Name - " + Room_names );
+       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+       document.getElementById("output").innerHTML += row;
       });});}
 getData();
+
+
+
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html"
+}
